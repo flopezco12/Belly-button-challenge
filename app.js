117e98c9058e24e2e46a469593ed19735d1e9fb1
flopezco12 <141666916+flopezco12@users.noreply.github.com>
2023-12-04 17:31:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const url = "samples.json";
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.";
 
 function init(){ 
     d3.json(url).then(function(alldata){
